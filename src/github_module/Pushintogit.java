65f4d2025f40091ff4e85e9e88c7147b979b65f6
Ravi Kumar <Ravi Kumar@RaviKumar-PC>
2017-07-23 03:39:49 -0700

    Verion 1

---
+++ src/github_module/Pushintogit.java
@@ -0,0 +1,12 @@
+package github_module;
+
+import org.testng.annotations.Test;
+
+public class Pushintogit {
+	@Test
+	public void smoketest()
+	{
+		System.out.println("Hi storing into git");
+	}
+
+}
